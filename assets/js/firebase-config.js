22f306992dbf7e26c768608d3f30a221eec08ca3
abouafnan-cmd <m.abou.afnan@gmail.com>
2026-07-27 01:59:29 +0100

    Add files via upload

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,17 +1,8 @@
-// استدعاء مكتبات Firebase الأساسية
+// استدعاء مكتبات Firebase عبر الروابط المباشرة للمتصفح
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, query, where, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
-// إعدادات مشروعك الخاص (ضع بياناتك هنا)
-const firebaseConfig = {
-    apiKey: "// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// إعدادات مشروعك الخاص (تم إدراج بياناتك)
 const firebaseConfig = {
   apiKey: "AIzaSyA0vuu9Wi3ubo-jHsJXXxTdjyVqIyNyF_Q",
   authDomain: "arabic-edu-platform.firebaseapp.com",
@@ -22,19 +13,9 @@ const firebaseConfig = {
   measurementId: "G-FTF8GK4B3G"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);",
-    authDomain: "project-id.firebaseapp.com",
-    projectId: "project-id",
-    storageBucket: "project-id.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef"
-};
-
 // تهيئة الاتصال
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// تصدير الأدوات
+// تصدير الأدوات لاستخدامها في واجهات المنصة
 export { db, collection, addDoc, getDocs, query, where, deleteDoc, doc };
